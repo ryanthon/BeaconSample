be3ad83363d970f29719f262537cdc130ec61a20
Ryan Khalili <rykhalil4@gmail.com>
2015-03-06 15:26:38 -0800

    Initial commit with sample code.

--- BeaconSample/ViewController.swift
+++ BeaconSample/ViewController.swift
@@ -7,19 +7,60 @@
 //
 
 import UIKit
+import CoreLocation
+import CoreBluetooth
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, CBPeripheralManagerDelegate {
 
+    let beaconID = "CSR beacon"
+    let beaconUUID = "4B5B9305-BA7F-4E69-B985-FB505253D81F"
+    
+    var cbManager: CBPeripheralManager!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        cbManager = CBPeripheralManager( delegate: self, queue: nil )
+    }
+    
+    func startAdvertisingAsBeacon() {
+        let uuid = NSUUID( UUIDString: beaconUUID )
+        let beaconRegion = CLBeaconRegion( proximityUUID: uuid, identifier: beaconID )
+        beaconRegion.notifyEntryStateOnDisplay = true
+        
+        let peripheralData = beaconRegion.peripheralDataWithMeasuredPower( nil )
+        cbManager.startAdvertising( peripheralData )
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    func peripheralManagerDidStartAdvertising(peripheral: CBPeripheralManager!, error: NSError!) {
+        
+        println( "Now advertising as beacon!" )
+    }
+    
+    func peripheralManagerDidUpdateState(peripheral: CBPeripheralManager!) {
+        switch peripheral.state {
+            case CBPeripheralManagerState.Unknown:
+                print( "Unknown" )
+            case CBPeripheralManagerState.Resetting:
+                print( "Resetting" )
+            case CBPeripheralManagerState.Unsupported:
+                print( "Unsupported" )
+            case CBPeripheralManagerState.Unauthorized:
+                print( "Unsupported" )
+            case CBPeripheralManagerState.PoweredOn:
+                print( "Powered On" )
+                startAdvertisingAsBeacon()
+            case CBPeripheralManagerState.PoweredOff:
+                print( "Powered Off" )
+            default:
+                print( "Default" )
+        }
+    }
 }
 
